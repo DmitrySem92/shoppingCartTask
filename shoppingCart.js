850a8cba1204fcf21a7791bcedb484ad67358c0b
Dmitry <DmitrySem92@gmail.com>
2024-02-26 21:09:04 +0300

    added comments

--- shoppingCart.js
+++ shoppingCart.js
@@ -2,12 +2,14 @@ const shoppingCart = {
     items: [],
     total: 0,
   
+    // Метод добавления товара
     addItem: function(name, price, quantity) {
       const newItem = { name, price, quantity };
       this.items.push(newItem);
       this.total += price * quantity;
     },
   
+    // Метод удаления товара
     removeItem: function(name) {
       const indexToRemove = this.items.findIndex(item => item.name === name);
       if (indexToRemove !== -1) {
@@ -16,6 +18,7 @@ const shoppingCart = {
       }
     },
   
+    // Метод обновления количества товара
     updateQuantity: function(name, newQuantity) {
       const itemToUpdate = this.items.find(item => item.name === name);
       if (itemToUpdate) {
@@ -25,18 +28,21 @@ const shoppingCart = {
       }
     },
   
+    // Метод расчета общей стоимости
     calculateTotal: function() {
       this.total = this.items.reduce((sum, item) => sum + item.price * item.quantity, 0);
     },
   
+    // Метод очистки корзины
     clearCart: function() {
       this.items = [];
       this.total = 0;
     },
   
+    // Дополнительное задание: метод применения скидки
     applyDiscount: function(discountCode) {
+      // Логика применения скидки
+      // В данном примере просто уменьшим total на 10%
       this.total *= 0.9;
     },
-  };
-  
-  
+  };  
